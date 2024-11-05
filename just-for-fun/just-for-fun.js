9d55a0dbb73b1187c022d80456b7279c3fe09006
Kenneth Duron <kennethduron.paz@gmail.com>
2024-11-05 10:32:01 -0600

    Finished the homework

---
+++ just-for-fun/just-for-fun.js
@@ -0,0 +1,11 @@
+let equalButton = document.getElementById("doTheCalculation");
+
+equalButton.addEventListener("click", (value) => {
+    // Get the number 1 value
+
+    // Get the number 2 value
+
+    // Do the addition
+
+    // return to p id "result" the result of the addition
+});
