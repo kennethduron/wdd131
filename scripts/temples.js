330717b573d39ee00a9a5c5892c0e69c2c25a5cf
Kenneth Duron <kennethduron.paz@gmail.com>
2024-11-08 05:54:50 -0600

    week2 completed

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(){
 const mainnav = document.querySelector(".navigation")
 const hambutton = document.querySelector("#menu")
 
-hambutton.addEventListener("click", () =>{
+hambutton.addEventListener("click", function() {
     mainnav.classList.toggle("show");
     hambutton.classList.toggle("show");
 });
